a73a8027f88d675d195e34c1d7a6a0a0720b41b3
wonsun <98735772+ws1811@users.noreply.github.com>
2023-11-01 16:08:30 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,16 @@
+-- 2022년 10월에 작성된
+-- 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일 조회
+-- 댓글 작성일 기준 오름차순 정렬 -> 게시글 제목 기준 오름차순 정렬
+SELECT b.title
+      ,b.board_id
+      ,r.reply_id
+      ,r.writer_id
+      ,r.contents
+      ,to_char(r.created_date, 'YYYY-MM-DD') as created_date
+FROM   used_goods_board b
+INNER JOIN   used_goods_reply r
+ON     b.board_id = r.board_id
+WHERE  extract(year from b.created_date) = 2022
+   and extract(month from b.created_date) = 10
+ORDER BY r.created_date asc, 
+         b.title; 
