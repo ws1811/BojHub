07c4709857aa2b9274088fd2d7dfce1199d15368
wonsun <98735772+ws1811@users.noreply.github.com>
2023-11-15 09:16:42 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,45 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    
+    public static void dfs(String begin, String target, String[] words, int cnt) {
+        // target으로 바꿀 수 있다면 -> 바로 return
+        if (begin.equals(target)) 
+        {
+            answer = cnt;
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++)
+        {
+            if (visited[i]) 
+            {
+                continue;
+            }
+
+            int sameSpell = 0;    // 같은 스펠링 몇개인지 세기
+            for (int j = 0; j < begin.length(); j++) 
+            {
+                if (begin.charAt(j) == words[i].charAt(j)) 
+                {
+                    sameSpell++;
+                }
+            }
+
+            if (sameSpell == begin.length() - 1)
+            {  // 한글자 빼고 모두 같은 경우 
+                visited[i] = true;
+                dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
