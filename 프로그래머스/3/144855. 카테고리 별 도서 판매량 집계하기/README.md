66658343ac6a96d454e67dcf8d04cd66777ea5b8
wonsun <98735772+ws1811@users.noreply.github.com>
2024-01-03 15:12:09 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 카테고리 별 도서 판매량 집계하기 - 144855 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 11월 1일 14:33:7
+2024년 1월 3일 15:12:8
 
 ### 문제 설명
 
--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,13 +1,13 @@
 -- 2022년 1월의 카테고리 별 도서 판매량을 합산하고
 -- 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력
 -- 결과는 카테고리명을 기준으로 오름차순 정렬
-
 SELECT  b.category
         ,sum(s.sales) as TOTAL_SALES
-FROM    BOOK b
-JOIN    BOOK_SALES s
-ON      b.book_id = s.book_id
-WHERE   extract(year from s.sales_date) = 2022
-        and extract(month from s.sales_date) = 1
-GROUP BY b.category
-ORDER BY b.category 
+FROM    book_sales s
+JOIN    book b
+ON      s.book_id = b.book_id
+WHERE   to_char(s.sales_date, 'YYYY-MM') = '2022-01'
+GROUP BY
+        b.category
+ORDER BY
+        b.category
