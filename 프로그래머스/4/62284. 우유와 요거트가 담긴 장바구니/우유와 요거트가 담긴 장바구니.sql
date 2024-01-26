ffa286fdb326fa2eb7ec5c78ac7f37fbac9debf3
wonsun <98735772+ws1811@users.noreply.github.com>
2024-01-26 21:51:54 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,10 +1,10 @@
--- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성
+-- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성해주세요. 
 -- 이때 결과는 장바구니의 아이디 순으로 나와야 합니다.
-
-SELECT      cart_id
-FROM        cart_products
-WHERE       NAME = 'Milk'
-INTERSECT
-SELECT      cart_id
-FROM        cart_products
-WHERE       name = 'Yogurt'
+SELECT  cart_id
+FROM    cart_products
+WHERE   cart_id IN (SELECT  cart_id
+                    FROM    cart_products
+                    WHERE   name = 'Yogurt')
+AND     name = 'Milk'
+ORDER BY
+        cart_id
