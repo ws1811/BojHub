75e927768e6885b1d53df45a5f06ea8482b60542
wonsun <98735772+ws1811@users.noreply.github.com>
2024-02-09 21:46:34 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 - 151139 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 11월 4일 11:30:45
+2024년 2월 5일 21:46:33
 
 ### 문제 설명
 
--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,8 +1,14 @@
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 
+-- 대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지 
+-- 총 대여 횟수가 5회 이상인 자동차들에 대해서 
+-- 해당 기간 동안의 월별 자동차 ID 별 총 대여 횟수(컬럼명: RECORDS) 리스트를 출력하는 SQL문을 작성해주세요. 
+-- 결과는 월을 기준으로 오름차순 정렬하고, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬해주세요. 
+-- 특정 월의 총 대여 횟수가 0인 경우에는 결과에서 제외해주세요.
 -- 코드를 입력하세요
 
 SELECT      extract(month from start_date) as month
             ,car_id
-            ,count(*) as records
+            ,count(start_date) as records
 FROM        car_rental_company_rental_history
 WHERE       car_id in (
                         SELECT      car_id
