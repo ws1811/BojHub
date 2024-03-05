34d8cc9cd25a6cd637b6614549d5efa7b2f70e3f
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-05 09:01:08 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,15 +1,24 @@
--- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블
--- ANIMAL_OUTS 테이블은 동물 보호소에서 입양 보낸 동물의 정보를 담은 테이블
 -- 아직 입양을 못 간 동물 중, 
--- 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성
--- 결과는 보호 시작일 순으로 조회
-SELECT  *
-FROM    (SELECT  i.name
-                ,i.datetime
+-- 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 보호 시작일 순으로 조회해야 합니다
+SELECT  name
+        ,datetime
+FROM
+       (
+        SELECT  i.name  AS name
+                ,i.datetime AS datetime
+                ,o.datetime AS d
         FROM    animal_ins i
         LEFT
         JOIN    animal_outs o
         ON      i.animal_id = o.animal_id
-        WHERE   o.animal_id IS NULL
-        ORDER BY i.datetime)
-WHERE   rownum <=3
+        WHERE   o.name IS NULL
+        AND     i.name IS NOT NULL
+        ORDER BY
+                i.datetime
+        )
+WHERE   ROWNUM <= 3
+ORDER BY    
+        datetime
+    ;
+
