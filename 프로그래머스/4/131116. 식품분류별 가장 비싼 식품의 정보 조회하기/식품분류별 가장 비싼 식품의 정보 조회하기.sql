c07eaa92583d0a4d4bbc5c2d98571acf5891227f
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-10 13:34:00 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,16 +1,20 @@
--- 식품분류별로 가격이 제일 비싼 식품의 
--- 분류, 가격, 이름을 조회하는 SQL문을 작성해주세요
--- 식품분류가 '과자', '국', '김치', '식용유'인 경우만 출력
---  결과는 식품 가격을 기준으로 내림차순 정렬해주세요.
-SELECT      category
-            ,price as MAX_PRICE
-            ,product_name
-FROM        food_product 
-WHERE       (category, price) in    (SELECT      category
-                                                ,max(price) as max_price
-                                    FROM        food_product
-                                    WHERE       category in ('과자', '국','김치','식용유')
-                                    GROUP BY    category)
-ORDER BY    max_price desc;
-
-
+-- FOOD_PRODUCT 테이블에서 
+-- 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름을 조회하는 SQL문을 작성해주세요. 
+-- 이때 식품분류가 '과자', '국', '김치', '식용유'인 경우만 출력시켜 주시고
+-- 결과는 식품 가격을 기준으로 내림차순 정렬해주세요.
+SELECT  category
+        ,price  AS MAX_PRICE
+        ,product_name
+FROM    food_product
+WHERE   (category, price) IN
+        (
+            SELECT  category
+                    ,MAX(price) as MAX_PRICE
+            FROM    food_product
+            GROUP BY
+                     category
+         )
+AND     category IN ('과자', '국', '김치', '식용유')
+ORDER BY
+        price desc
+;
