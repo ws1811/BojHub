bef5024d29137e3bc7643e2625f4001e91ac6f10
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-20 22:11:27 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
@@ -1,6 +1,6 @@
 # [level 4] 보호소에서 중성화한 동물 - 59045 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 11월 0일 14:19:56
+2024년 03월 20일 22:11:23
 
 ### 문제 설명
 
--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,9 +1,14 @@
--- 보호소에 들어올 당시에는 중성화1되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의
--- 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성
-SELECT      i.animal_id, i.animal_type, i.name
-FROM        animal_ins i
-JOIN        animal_outs o
-ON          i.animal_id = o.animal_id
-WHERE       i.sex_upon_intake like '%Intact%' 
-and         o.sex_upon_outcome not like '%Intact%' 
-ORDER BY    o.animal_id
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다.
+-- 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의
+-- 아이디와 생물 종, 이름을 조회하는 
+-- 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+SELECT  i.animal_id
+        ,i.animal_type
+        ,i.name
+FROM    animal_ins i
+JOIN    animal_outs o
+ON      i.animal_id = o.animal_id
+WHERE   i.sex_upon_intake like '%Intact%'
+AND     o.sex_upon_outcome IN ('Neutered Male', 'Spayed Female')
+ORDER BY
+        i.animal_id
