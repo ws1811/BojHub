ccbb4a5313dd0e5aa61f38e364c78bfe2d2665cc
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-20 15:05:02 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,12 +1,10 @@
--- in.datetime   out.dataetime
--- 보호 시작일보다 입양일이 더 빠른 동물의
--- animal_id ,name
--- 아이디와 이름을 조회하는 SQL문을 작성
--- 결과는 보호 시작일이 빠른 순으로 조회
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
 SELECT  i.animal_id
         ,i.name
 FROM    animal_ins i
 JOIN    animal_outs o
 ON      i.animal_id = o.animal_id
-WHERE   i.datetime > o.datetime
-ORDER BY i.datetime asc
+WHERE   o.datetime < i.datetime
+ORDER BY 
+        i.datetime
