b2a0855b122d04e40125e75eba321f8c692de144
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-21 17:54:04 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,16 +1,21 @@
--- FOOD_PRODUCT와 FOOD_ORDER 테이블에서 
--- 생산일자가 2022년 5월인 식품들의 식품 ID, 식품 이름, 총매출을 조회하는 SQL문을 작성해주세요. 
--- 이때 결과는 총매출을 기준으로 내림차순 정렬해주시고 
+-- FOOD_PRODUCT와 FOOD_ORDER 테이블에서
+-- 생산일자가 2022년 5월인 식품들의 
+-- 식품 ID, 식품 이름, 총매출을 조회하는 SQL문을 작성해주세요.
+-- 이때 결과는 총매출을 기준으로 내림차순 정렬해주시고
 -- 총매출이 같다면 식품 ID를 기준으로 오름차순 정렬해주세요.
 SELECT  p.product_id
         ,p.product_name
-        ,sum(p.price * o.amount) as total_sales
-FROM    food_order o
-JOIN    food_product p
-ON      o.product_id = p.product_id
-WHERE   to_char(produce_date, 'YYYY-MM') = '2022-05'
-GROUP BY
-        p.product_id,p.product_name
-ORDER BY    
+        ,sq.amount * p.price AS TOTAL_SALES
+FROM    food_product p
+JOIN                   (
+                       SELECT      o.product_id
+                                   ,SUM(o.amount) AS amount
+                       FROM        food_order o
+                       WHERE       to_char(o.produce_date, 'YYYY-MM') = '2022-05'
+                       GROUP BY
+                                   o.product_id
+                       ) sq
+ON      p.product_id = sq.product_id
+ORDER BY
         total_sales desc
-        ,product_id;
+        ,p.product_id
