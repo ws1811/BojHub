a40374973151b69371cf854fb77be7b2cf955e23
wonsun <98735772+ws1811@users.noreply.github.com>
2024-03-21 10:08:54 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,19 +1,17 @@
--- USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서
--- 완료된 중고 거래의 총금액이 70만 원 이상인 사람의
--- 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성
--- 결과는 총거래금액을 기준으로 오름차순 정렬
+-- USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성해주세요. 결과는 총거래금액을 기준으로 오름차순 정렬해주세요.
 SELECT  u.user_id
         ,u.nickname
-        ,b.total_sales
-FROM     (SELECT  writer_id
-                ,sum(price) as total_sales
-         FROM    used_goods_board 
-         WHERE   status='DONE'
-         GROUP BY writer_id
-         HAVING  sum(price) >= 700000) b
-JOIN    used_goods_user u
-ON      u.user_id = b.writer_id
-ORDER BY total_sales
-
-
-
+        ,sq.total_sales
+FROM    used_goods_user u
+JOIN    (
+        SELECT  b.writer_id     AS  USER_ID      
+                ,sum(b.price)   AS  TOTAL_SALES
+        FROM    used_goods_board b
+        WHERE   b.status = 'DONE'
+        GROUP BY
+                b.writer_id
+        HAVING  sum(b.price) >= 700000
+        ) sq
+ON      u.user_id = sq.user_id
+ORDER BY
+        sq.total_sales
